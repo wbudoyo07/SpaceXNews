a91a9f39cc6f28aacf8a5afb6c06256cce0cc001
William Budoyo <williambu07@gmail.com>
2018-09-20 16:14:06 -0400

    heroku

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 
 const PORT = 8080;
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = 'mongodb://heroku_dm9tgqfs:NGVg8V4kUpDUBpY.mlab.com:11063/heroku_dm9tgqfs'
+// "mongodb://localhost/spaceX";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Initialize Express
 const app = express();
@@ -26,17 +34,6 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/spaceX", { useNewUrlParser: true });
-
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = "mongodb://<dbuser>:<dbpassword>@ds111063.mlab.com:11063/heroku_dm9tgqfs"|| "mongodb://localhost/mongoHeadlines";
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI)
 
 // Start the server
 app.listen(PORT, function() {
