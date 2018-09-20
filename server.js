59e413e243fb6e0ff4f9774e4509ad8db12d7bb5
William Budoyo <williambu07@gmail.com>
2018-09-20 16:44:31 -0400

    deploy

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 
 const PORT = 8080;
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// If deployed, use the deployed database. Otherwise use the local spaceX database
 var MONGODB_URI = "mongodb://heroku_2dbg414q:ssldsojn5ko436a99njqnd9k9o@ds163842.mlab.com:63842/heroku_2dbg414q"||"mongodb://localhost/spaceX";
-// "mongodb://localhost/spaceX";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
